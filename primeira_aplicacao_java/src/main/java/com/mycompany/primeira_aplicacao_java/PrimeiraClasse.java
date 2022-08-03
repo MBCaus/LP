f938010a095f734d10b7cb8bffa8b0156e0d4fe2
Matheus Barizon Caus <99812182+MBCaus@users.noreply.github.com>
2022-08-03 14:39:09 -0300

    Primeira Aula LP
    Introdução

---
+++ primeira_aplicacao_java/src/main/java/com/mycompany/primeira_aplicacao_java/PrimeiraClasse.java
@@ -0,0 +1,11 @@
+package com.mycompany.primeira_aplicacao_java;
+
+public class PrimeiraClasse {
+
+    public static void main(String[] args) {
+        
+        System.out.println("");
+        
+    }
+
+}
