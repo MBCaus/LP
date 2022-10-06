692e3132ccb91b6ae37160a6f60b6cef97f1b977
MBCaus <matheus.caus@sptech.school>
2022-10-06 20:02:43 -0300

    Atualização treino AC2

--- atividade-04-hospital/src/main/java/com/sptech/atividade/com/testes/Hospital.java
+++ atividade-04-hospital/src/main/java/com/sptech/atividade/com/testes/Hospital.java
@@ -22,22 +22,108 @@ public class Hospital {
         return quantidadeDeHoraExtra;
     }
 
+    public Medico getDoutor() {
+        return doutor;
+    }   
+
+    Medico doutor = new Medico(0, "", "");
+
     public void realizarPagamento(Medico medico, Double valorPago) {
-        
-        
-        
+
+        if (medico != null && valorPago != null) {
+
+            if (doutor.getAtivo() == true) {
+
+                if (valorPago > 0) {
+                    
+                    doutor.setSalario(doutor.getSalario() + valorPago);
+                    
+                    quantidadeDePagamentos++;
+                    
+                } else {
+
+                    System.out.println("Digite um valor positivo.");
+
+                }
+
+            } else {
+
+                System.out.println("O Medico responsavel deve estar ativo.");
+
+            }
+
+        } else {
+
+            System.out.println("Digite um valor antes.");
+
+        }
+
     }
-    
+
     public void realizarPagamento(Medico medico, Double valorPago, Double horaExtra) {
-    
         
+        if(medico != null && valorPago != null) {
+        
+            if (doutor.getAtivo() == true) {
+
+                if (valorPago > 0) {
+                    
+                    doutor.setSalario(doutor.getSalario() + valorPago);
+                    
+                    quantidadeDePagamentos++;
+                    
+                    if(horaExtra != null && horaExtra > 0) {
+                    
+                        doutor.setSalario(doutor.getSalario() + valorPago);
+                        
+                        quantidadeDeHoraExtra++;
+                    
+                    }else {
+                    
+                        System.out.println("Digite um valor valido.");
+                        
+                    }
+                    
+                } else {
+
+                    System.out.println("Digite um valor positivo.");
+
+                }
+
+            } else {
+
+                System.out.println("O Medico responsavel deve estar ativo.");
+
+            }
+            
+        }else {
+            
+            System.out.println("Adicione um valor.");
+        
+        }
         
     }
-    
+
     public void desligarMedico(Medico medico) {
-    
         
-    
+        if(medico != null) {
+            
+            if(doutor.getAtivo() == true) {
+            
+                doutor.setAtivo(Boolean.FALSE);
+                
+            }else{
+            
+                System.out.println("O médico já esta desativado.");
+                
+            }
+            
+        }else {
+        
+            System.out.println("Adicione o nome de um médico.");
+            
+        }
+        
     }
 
 }
