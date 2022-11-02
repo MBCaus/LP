2d7143377808120332397001c1381bb7ae8a248b
MBCaus <matheus.caus@sptech.school>
2022-11-01 21:31:09 -0300

    atividade herança

---
+++ projeto1-heranca-completo/src/main/java/school/sptech/projeto1/heranca/ProdutoInternacional.java
@@ -0,0 +1,19 @@
+package school.sptech.projeto1.heranca;
+
+public class ProdutoInternacional extends Produto{
+
+    public ProdutoInternacional(Integer id, String nome, String categoria, Double preco) {
+        super(id, nome, categoria, preco);
+    }
+
+    @Override
+    public Double getPreco() {
+        Double precoTotal = (super.getPreco() * 0.6) + super.getPreco();
+        return precoTotal;
+    }
+
+    /* 
+        1. Essa classe deve ser herdeira de produto;
+        2. O método "getPreco" (getter padrão) deverá ser sobreescrito e possuir um acréscimo de 60%;
+     */
+}
